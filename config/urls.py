4dabf9018e113b54afb3efc2a546d4f9d25ac5c5
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-07-03 23:25:10 +0600

    heroku

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings 
+from django.conf.urls.static import static
+from django.views.static import serve 
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -22,3 +27,10 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), 
+    urlpatterns += url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}), 
