6c0c89a154ae9171f16a5d93e6274db8bd1672f8
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-07-03 23:01:18 +0600

    added documentation

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,24 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="MyDent API",
+      default_version='v1',
+      description="API documentation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rest_api.urls'))
+    path('', include('rest_api.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- rest_api/views/client_users.py
+++ rest_api/views/client_users.py
@@ -3,14 +3,23 @@ from customUser.models import User
 from rest_api.serializers.client_user_serializers import ClientUsersSerializer, ClientRegisterSerializer
 from rest_api.send_mail import send_confirmation_email
 from rest_framework import status, response
+from drf_yasg.utils import swagger_auto_schema
 
 class ClientUsersView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = ClientUsersSerializer
 
+    @swagger_auto_schema(operation_description='List client users', tags=['Client User'],
+                         security=[])
+    def get(self, request):
+        return self.list(request)
+
+
 class ClientUserRegisterView(generics.CreateAPIView):
     serializer_class = ClientRegisterSerializer
 
+    @swagger_auto_schema(operation_description='Registration client user', tags=['Client User'],
+                         security=[])
     def post(self, request):
         serializer = ClientRegisterSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
