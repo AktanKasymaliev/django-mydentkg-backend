6a9843c62d58fff0636a3be575081086223d987d
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2021-08-20 13:55:09 +0600

    changed access token

--- config/settings.py
+++ config/settings.py
@@ -200,7 +200,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=43800),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1000000),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
